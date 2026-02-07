b4bf94419a91f32bf0f93e26be86a3316352b560
Tejas Dixit <tejasd910@gmail.com>
2026-02-08 00:14:59 +0530

    Singly LL operation

---
+++ 05LinkedList/BasicSl.java
@@ -0,0 +1,97 @@
+class Linked{
+    Node head;
+
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+    public void printList(){
+        if(head == null){
+            System.out.println("This list is empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println("This list is empty");
+            return;
+        }
+        head = head.next;
+    }
+    public void deleteLast(){
+        if(head == null){
+            System.out.println("This list is empty");
+            return;
+        }
+        if(head.next==null){
+            head = null;
+            return;
+        }
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null; 
+    }
+    public void reverse(){
+        if(head == null || head.next ==null){
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode!=null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode; 
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next=null;
+        head=prevNode;
+    }
+    public static void main(String[] args) {
+        Linked list = new Linked();
+        list.printList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addFirst("this");
+        list.printList();
+        list.addLast("list");
+        list.printList();
+        list.reverse();
+        list.printList();
+        
+    }
+}
