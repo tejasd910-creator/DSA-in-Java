29e319f394542f6624aca46626fa3530932aaa7e
Tejas Dixit <tejasd910@gmail.com>
2026-02-19 22:27:50 +0530

    234. Palindrome link list

---
+++ 05LinkedList/02MediumI/06palindromeList.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head.next == null)
+            return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode next = slow.next;
+        slow.next = null;
+        while (next != null) {
+            ListNode temp = next.next;
+            next.next = slow;
+            slow = next;
+            next = temp;
+        }
+
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val)
+                return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
