243a7ca46199f17e00fe0c75d31e34c461841a4f
Tejas Dixit <tejasd910@gmail.com>
2026-02-22 00:58:15 +0530

    2095 Leetcode

---
+++ 05LinkedList/02MediumI/09deleteMiddle.java
@@ -0,0 +1,35 @@
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null) return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = slow;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+
+        return head;
+    }
+}
