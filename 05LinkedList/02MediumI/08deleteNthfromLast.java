2504e4de1f8e8f312b59122257b0616408044e27
Tejas Dixit <tejasd910@gmail.com>
2026-02-24 00:39:59 +0530

    19 Leetcode

--- 05LinkedList/02MediumI/08deleteNthfromLast.java
+++ 05LinkedList/02MediumI/08deleteNthfromLast.java
@@ -15,6 +15,7 @@ class ListNode {
         this.next = next;
     }
 }
+
 class Solution {
     public ListNode removeNthFromEndI(ListNode head, int n) {
         if (head.next == null)
@@ -38,5 +39,54 @@ class Solution {
         curr.next = temp;
 
         return head;
+    }
+
+    public ListNode removeNthFromEndII(ListNode head, int n) {
+        if (head.next == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = slow;
+        int dist = 1;
+
+        while (dist < n) {
+            fast = fast.next;
+            dist++;
+        }
+        while (fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        if (slow == head)
+            return head.next;
+        prev.next = slow.next;
+
+        return head;
+    }
+
+    public ListNode removeNthFromEndIII(ListNode head, int n) {
+        // Dummy node to handle edge cases like removing the first node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // Move first pointer n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        // Move both pointers until first reaches the end
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // Remove the nth node from end
+        second.next = second.next.next;
+
+        return dummy.next;
     }
 }
