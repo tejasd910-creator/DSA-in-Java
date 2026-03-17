15445a8ba7e3ce2649368177a357309635abc2ea
Tejas Dixit <tejasd910@gmail.com>
2026-03-17 23:55:00 +0530

    GFG all Possible Strings

---
+++ 06Recursion/02Subsequences/03allPossibleStrings.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+class Solution {
+    public List<String> AllPossibleStrings(String s) {
+        // Code here
+        List<String> result = new ArrayList<>();
+        subSequence(result, s, "", 0);
+        Collections.sort(result);
+        return result;
+    }
+    
+    private void subSequence(List<String> result, String s, String current, int idx){
+        if(idx == s.length()){
+            result.add(current);
+            return;
+        }
+        char curChar = s.charAt(idx);
+        
+        subSequence(result, s, current + curChar, idx + 1);
+        
+        subSequence(result, s, current, idx + 1);
+    }
+}
