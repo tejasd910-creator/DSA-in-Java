3128eb860b49105fb5e2b000d9749118782eccb6
Tejas Dixit <tejasd910@gmail.com>
2026-03-24 00:29:18 +0530

    Subsets 1

---
+++ 06Recursion/02Subsequences/08Subsets.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        findSubsets(nums, nums.length, 0, ans, new ArrayList<>());
+
+        return ans;
+    }
+
+    private void findSubsets(int[] nums, int n, int idx, List<List<Integer>> ans, List<Integer> ds){
+        if(n == 0) return;
+        if(idx == n){
+            ans.add(new ArrayList(ds));
+            return;
+        }
+
+        ds.add(nums[idx]);
+        findSubsets(nums, n, idx + 1, ans, ds);
+
+        ds.remove(ds.size() - 1);
+        findSubsets(nums, n, idx + 1, ans, ds);
+    }
+}
