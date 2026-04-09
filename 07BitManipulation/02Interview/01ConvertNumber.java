5693ae505f0b1a7e944d4efe6f3b051ba025161f
Tejas Dixit <tejasd910@gmail.com>
2026-04-10 00:02:30 +0530

    Minimum Bit Flips to Convert Number

---
+++ 07BitManipulation/02Interview/01ConvertNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count = 0;
+        int n = start ^ goal;
+        while(n != 0){
+            n &= (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+}
