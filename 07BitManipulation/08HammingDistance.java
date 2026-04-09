a271892a5c21f771c748324a39b825914fa342d7
Tejas Dixit <tejasd910@gmail.com>
2026-04-09 23:55:42 +0530

    Hamming Distance

---
+++ 07BitManipulation/08HammingDistance.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        int n = x ^ y;
+        while(n != 0){
+            n &= (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+}
