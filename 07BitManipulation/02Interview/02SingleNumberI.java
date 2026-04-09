ff803c3913b0d379750d4584adf6891e295fb853
Tejas Dixit <tejasd910@gmail.com>
2026-04-10 00:02:51 +0530

    Single Number 1

---
+++ 07BitManipulation/02Interview/02SingleNumberI.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+
+        for(int i : nums){
+            single ^= i;
+        }
+
+        return single;
+    }
+}
