c2c3b009a349d5bdc7e4f5d2eb1e7b4543668cb4
Tejas Dixit <tejasd910@gmail.com>
2026-04-17 23:24:14 +0530

    Power(x,n)

---
+++ 07BitManipulation/03AdvanceMath.java/05Power(x,n).java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+
+        while (N > 0) {
+            if ((N & 1) == 1)
+                result *= x;
+            x *= x;
+            N >>= 1;
+        }
+
+        return result;
+    }
+}
