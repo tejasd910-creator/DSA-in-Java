df7501d2250cd0d82d59d59bf2cdf55686c2e1eb
Tejas Dixit <tejasd910@gmail.com>
2026-05-04 23:57:59 +0530

    Next GReater Element 2

---
+++ 08Stack&Queue/02MonotonicStack.java/02NextGReaterElementII.java
@@ -0,0 +1,17 @@
+public int[] nextGreaterElements(int[] nums) {
+    int n = nums.length;
+    int[] res = new int[n];
+    Arrays.fill(res, -1);
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = 0; i < 2 * n; i++) {
+        while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+            res[stack.pop()] = nums[i % n];
+        }
+        if (i < n) {
+            stack.push(i);
+        }
+    }
+
+    return res;
+}
