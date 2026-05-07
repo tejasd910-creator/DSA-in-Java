f2b13ffaba657e7b0af4968cd12b24224b1cdeae
Tejas Dixit <tejasd910@gmail.com>
2026-05-07 23:53:17 +0530

    Next Greater Element II

--- 08Stack&Queue/02MonotonicStack.java/02NextGReaterElementII.java
+++ 08Stack&Queue/02MonotonicStack.java/02NextGReaterElementII.java
@@ -1,17 +1,45 @@
-public int[] nextGreaterElements(int[] nums) {
-    int n = nums.length;
-    int[] res = new int[n];
-    Arrays.fill(res, -1);
-    Stack<Integer> stack = new Stack<>();
+import java.util.*;
 
-    for (int i = 0; i < 2 * n; i++) {
-        while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
-            res[stack.pop()] = nums[i % n];
+class NextGreaterElement2 {
+
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        // Traverse array twice for circular behavior
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            int current = nums[i % n];
+
+            // Remove smaller or equal elements
+            while (!stack.isEmpty() && stack.peek() <= current) {
+                stack.pop();
+            }
+
+            // Fill answer only for first pass
+            if (i < n) {
+                if (!stack.isEmpty()) {
+                    result[i] = stack.peek();
+                }
+            }
+
+            // Push current element
+            stack.push(current);
         }
-        if (i < n) {
-            stack.push(i);
-        }
+
+        return result;
     }
 
-    return res;
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 1};
+
+        int[] ans = nextGreaterElements(nums);
+
+        System.out.println(Arrays.toString(ans));
+    }
 }
