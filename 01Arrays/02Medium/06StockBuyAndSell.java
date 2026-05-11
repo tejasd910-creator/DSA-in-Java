e5aad5033b8db27cfdeab9c80f7c4981484f23b6
Tejas Dixit <tejasd910@gmail.com>
2026-05-12 00:27:35 +0530

    Stock Buy And Sell

---
+++ 01Arrays/02Medium/06StockBuyAndSell.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int max = 0;
+        int buy = prices[0];
+
+        for(int i = 0; i < prices.length; i++){
+            if(buy > prices[i])
+               buy = prices[i];
+
+            max = Math.max(max, prices[i] - buy);
+        }
+        return max;
+    }
+}
