7bb185f647a0f4ebb42080c85be32b484944c886
Tejas Dixit <tejasd910@gmail.com>
2026-05-26 23:40:06 +0530

    Rotate Matrix By 90 Degree

---
+++ 01Arrays/02Medium/12RotateMatrix90Degree.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // taking transpose
+        for (int i = 0; i < row; i++) {
+            for (int j = i + 1; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // rotating row
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < (col) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][col - j - 1];
+                matrix[i][col - j - 1] = temp;
+            }
+        }
+
+    }
+}
