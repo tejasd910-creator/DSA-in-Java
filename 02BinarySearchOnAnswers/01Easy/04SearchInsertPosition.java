f0bf9ddcb7c5bb0207cea1d08edef9fd0cf24fda
Tejas Dixit <tejasd910@gmail.com>
2026-06-02 23:42:12 +0530

    Search Insert Position

---
+++ 02BinarySearchOnAnswers/01Easy/04SearchInsertPosition.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else {
+                ans = mid;
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
