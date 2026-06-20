adc94fc33702645dc7ba462cdaf5facfeae8cf1d
Tejas Dixit <tejasd910@gmail.com>
2026-06-20 23:48:18 +0530

    Largest Rectangle In Histgram

---
+++ 08Stack&Queue/02MonotonicStack.java/10LargestRectangleInHistogram.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class LargestRectangleHistogram {
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() &&
+                    currentHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int width;
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
