fe9de44dfedf071224015a0b1520726d6a1b5754
Tejas Dixit <tejasd910@gmail.com>
2026-06-22 23:48:35 +0530

    Find Square Number

---
+++ 02BinarySearchOnAnswers/02BsOnAnswers/01FindSquareRoot.java
@@ -0,0 +1,18 @@
+class Solution {
+    int floorSqrt(int n) {
+        // code here
+        int low = 0, high = n, ans = 0;
+         
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(mid*mid <= n){
+             ans = mid;
+             low = mid + 1;
+            }
+            else
+             high = mid - 1;
+        }
+        
+        return ans;
+    }
+} 
