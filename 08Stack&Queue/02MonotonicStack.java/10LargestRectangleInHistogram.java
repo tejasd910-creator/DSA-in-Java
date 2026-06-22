e8bb9aab6afd951864df44c3b4b852b7be766859
Tejas Dixit <tejasd910@gmail.com>
2026-06-23 01:04:57 +0530

    Largest Rectangle In Histogram

--- 08Stack&Queue/02MonotonicStack.java/10LargestRectangleInHistogram.java
+++ 08Stack&Queue/02MonotonicStack.java/10LargestRectangleInHistogram.java
@@ -29,4 +29,45 @@ class LargestRectangleHistogram {
 
         return maxArea;
     }
+
+    //Time optimal but not space (uses extra space for pse and nse array)
+    public int largestRectangleAreaII(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = Integer.MIN_VALUE;
+        int n = heights.length;
+
+        int[] pse = new int[n];
+        int[] nse = new int[n];
+
+        //PSE
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+
+            pse[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        //NSE
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+
+            nse[i] = stack.isEmpty() ? n : stack.peek();
+
+            stack.push(i);
+        }
+
+        for(int i = 0; i < n; i++){
+            int breadth = nse[i] - pse[i] - 1;
+
+            maxArea = Math.max(maxArea, breadth*heights[i]);
+        }
+
+        return maxArea;
+    }
 }
