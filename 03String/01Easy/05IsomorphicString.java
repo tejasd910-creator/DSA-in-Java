c19876259f5a53b533234e0c44cdcef42a8279dc
Tejas Dixit <tejasd910@gmail.com>
2026-07-09 23:35:13 +0530

    Isomorphic String

---
+++ 03String/01Easy/05IsomorphicString.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (mapST[a] == 0 && mapTS[b] == 0) {
+                mapST[a] = b;
+                mapTS[b] = a;
+            } else {
+                if (mapST[a] != b || mapTS[b] != a) return false;
+            }
+        }
+        return true;
+    }
+}
