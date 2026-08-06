fc80326f78a6d7b9191913fceba303d72142a2ec
Tejas Dixit <tejasd910@gmail.com>
2026-08-07 00:25:38 +0530

    Maximum Number  of Meeting in One Room

--- 11GreedyAlgo/02Medium/01MaximmMeetingInOneRoom.java
+++ 11GreedyAlgo/02Medium/01MaximmMeetingInOneRoom.java
@@ -11,7 +11,7 @@ class Solution {
         }
     }
 
-    public int maxMeetings(int[] start, int[] end) {
+    public int maxMeetingsNumber(int[] start, int[] end) {
         int n = start.length;
 
         List<Meeting> meetings = new ArrayList<>();
@@ -33,6 +33,45 @@ class Solution {
         }
 
         return count;
+    }
+
+    public ArrayList<Integer> maxMeetingsOrder(int[] s, int[] f) {
+        int n = s.length;
+
+        class Meeting {
+            int start, finish, idx;
+            Meeting(int start, int finish, int idx) {
+                this.start = start;
+                this.finish = finish;
+                this.idx = idx;
+            }
+        }
+
+        ArrayList<Meeting> meetings = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            meetings.add(new Meeting(s[i], f[i], i + 1));
+        }
+
+        Collections.sort(meetings, (a, b) -> {
+            if (a.finish != b.finish)
+                return a.finish - b.finish;
+            return a.idx - b.idx;
+        });
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int lastFinish = -1;
+
+        for (Meeting m : meetings) {
+            if (m.start > lastFinish) {
+                ans.add(m.idx);
+                lastFinish = m.finish;
+            }
+        }
+
+        Collections.sort(ans);
+        return ans;
     }
 }
 
