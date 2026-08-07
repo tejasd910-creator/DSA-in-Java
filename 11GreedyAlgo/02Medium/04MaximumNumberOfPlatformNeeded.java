18c14119850e9f1a6f868aba44de4d3cdec7dfda
Tejas Dixit <tejasd910@gmail.com>
2026-08-07 23:45:57 +0530

    Maximum Number of Platform Needed

---
+++ 11GreedyAlgo/02Medium/04MaximumNumberOfPlatformNeeded.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution{
+    public int findPlatform(int[] arr, int[] dep) {
+    int n = arr.length;
+
+    Arrays.sort(arr);
+    Arrays.sort(dep);
+
+    int platformsNeeded = 1;
+    int maxPlatforms = 1;
+
+    int i = 1, j = 0;
+
+    while (i < n && j < n) {
+        if (arr[i] <= dep[j]) {
+            platformsNeeded++;
+            maxPlatforms = Math.max(maxPlatforms, platformsNeeded);
+            i++;
+        } else {
+            platformsNeeded--;
+            j++;
+        }
+    }
+
+    return maxPlatforms;
+}
+}
