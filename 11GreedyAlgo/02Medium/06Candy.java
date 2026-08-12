be11dd5b5198eb137ce4d9a46cef4dbc2ede134e
Tejas Dixit <tejasd910@gmail.com>
2026-08-12 23:48:35 +0530

    Candy

---
+++ 11GreedyAlgo/02Medium/06Candy.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+
+        // Everyone gets at least 1 candy
+        Arrays.fill(candies, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Calculate total
+        int total = 0;
+        for (int candy : candies) {
+            total += candy;
+        }
+
+        return total;
+    }
+}
