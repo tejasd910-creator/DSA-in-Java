f08b700b8f0ec9f16b0d6860ef5f17c23bc30c63
Tejas Dixit <tejasd910@gmail.com>
2026-09-11 23:14:49 +0530

    Top View of BT

--- 12BinaryTree/02Medium/09TopView.java
+++ 12BinaryTree/02Medium/09TopView.java
@@ -11,46 +11,58 @@ class Node {
     }
 }
 
-class Solution {
-    static class Pair {
+class Pair{
         Node node;
-        int hd;
-
-        Pair(Node node, int hd) {
-            this.node = node;
-            this.hd = hd;
+        int col;
+        
+        Pair(Node _node, int _col){
+            this.node = _node;
+            this.col = _col;
         }
     }
 
+
+class Solution {
+    // static class Pair{
+    //     Node node;
+    //     int col;
+        
+    //     Pair(Node _node, int _col){
+    //         this.node = _node;
+    //         this.col = _col;
+    //     }
+    // }
+    
     public ArrayList<Integer> topView(Node root) {
+        // code here
         ArrayList<Integer> ans = new ArrayList<>();
-
-        if (root == null)
-            return ans;
-
+        
+        if(root == null)
+           return ans;
+           
         Queue<Pair> q = new LinkedList<>();
         TreeMap<Integer, Integer> map = new TreeMap<>();
-
+        
         q.offer(new Pair(root, 0));
-
-        while (!q.isEmpty()) {
+        
+        while(!q.isEmpty()){
             Pair p = q.poll();
-
-            if (!map.containsKey(p.hd)) {
-                map.put(p.hd, p.node.data);
+            
+            if(!map.containsKey(p.col)){
+                map.put(p.col, p.node.data);
             }
-
-            if (p.node.left != null) {
-                q.offer(new Pair(p.node.left, p.hd - 1));
+            
+            if(p.node.left != null){
+                q.offer(new Pair(p.node.left, p.col - 1));
             }
-
-            if (p.node.right != null) {
-                q.offer(new Pair(p.node.right, p.hd + 1));
+            
+            if(p.node.right != null){
+                q.offer(new Pair(p.node.right, p.col + 1));
             }
+            
         }
-
         ans.addAll(map.values());
-
+        
         return ans;
     }
 }
